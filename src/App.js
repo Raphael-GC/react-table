e8fcaeeb749aa8f88f2d8097d101cd7b83f4435b
Raphael-GC <raphaelgc.i9@gmail.com>
2021-03-16 23:47:44 -0300

    Data update

--- src/App.js
+++ src/App.js
@@ -1,33 +1,19 @@
-//import logo from './logo.svg';
-//import './App.css';
 import React from 'react';
 import Table from './Table.js';
-
-//function App() {
-//  return (
-//    <div className="App">
-//      <header className="App-header">
-//        <img src={logo} className="App-logo" alt="logo" />
-//      </header>
-//    </div>
-//  );
-//}
-
-
 
 function App() {
   const columns = [
     { path: "id",   name: "ID" },
     { path: "name", name: "Name" },
     { path: "age",  name: "Age" },
-    { path: "favFruit",  name: "Favourite Fruit" },
+    { path: "element",  name: "Element" },
   ];
   
   const data = [
-    { id: 1, name: 'Kate',  age: 25, favFruit: '🍏' },
-    { id: 2, name: 'Tom',   age: 23, favFruit: '🍌' },
-    { id: 3, name: 'Ann',   age: 26, favFruit: '🍊' },
-    { id: 4, name: 'Jack',  age: 21, favFruit: '🍒' }
+    { id: 1, name: 'Kate',  age: 25, element: '🔥' },
+    { id: 2, name: 'Tom',   age: 23, element: '💨' },
+    { id: 3, name: 'Ann',   age: 26, element: '💦' },
+    { id: 4, name: 'Jack',  age: 21, element: '🌳' }
   ];
   return (
     
